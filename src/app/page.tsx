0593ed04bc24ee2bc0210742d6ab216cdcd166d9
camilaramirez4 <camila.ramirez4@udea.edu.co>
2023-10-05 19:53:43 -0500

    Prueba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home () {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <button>BtnTest</button>
       <span>HOLI</span>
+      <span>Pruebita</span>
     </main>
   )
 }
