2d54de7733327fa4d29fd72accb765b9f6f1d512
Jeison Moncayo <desarrollo5ude@udea.edu.co>
2023-10-30 16:56:04 -0500

    Agregada tarjeta de descuentos

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ export default function DashboardLayout ({
   children: React.ReactNode
 }) {
   return (
-    <section className="flex w-full gap-2 p-1">
+    <section className="flex w-full gap-2 p-1 bg-[#EDEDED] min-h-[calc(100vh-6.75rem)]">
       <aside className="flex flex-col items-center gap-5 p-2 w-1/5">
         <h1 className="text-3x">Welcome to OFFCORS</h1>
         <p className="text-2x">The best way to manage your projects</p>
--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -2,7 +2,7 @@ import { ButtonAuth } from '@/components/common/button-auth'
 
 export function Header () {
   return (
-    <header className="w-full flex items-center justify-between gap-5 px-8 py-3 bg-[#1D1D1E] text-[#FFDD00]">
+    <header className="w-full h-16 flex items-center justify-between gap-5 px-8 py-3 bg-[#1D1D1E] text-[#FFDD00]">
       <h1 className="text-3x">OFFCORS</h1>
       <ButtonAuth />
     </header>
