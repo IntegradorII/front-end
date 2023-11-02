9df95ac53ab9cdb96f6ecd0d6e7740485e6438c6
camilaramirez4 <camila.ramirez4@udea.edu.co>
2023-11-01 23:54:52 -0500

    Correccion de detalles

--- src/app/dashboard/discounts/page.tsx
+++ src/app/dashboard/discounts/page.tsx
@@ -5,44 +5,44 @@ const discounts: Discount[] = [
   {
     id: 1,
     type: 'oferta',
-    title: 'Envio gratis',
-    description: '¡Envio sin costo en tu proximo pedido!',
-    expirationDate: '2021-08-31'
+    title: 'Envío gratis',
+    description: '¡Envío sin costo en tu próximo pedido!',
+    expirationDate: '28 de Agosto de 2021'
   },
   {
     id: 2,
     type: 'oferta',
     value: '20%',
-    description: '¡Recibe un 20% de descuento en tu proxima compra!',
-    expirationDate: '2021-08-31'
+    description: '¡Recibe un 20% de descuento en tu próxima compra!',
+    expirationDate: '28 de Agosto de 2021'
   },
   {
     id: 3,
-    type: 'cupon',
-    value: '$ 15000',
-    description: '¡Obten un descuento de $15000 pesos en cualquiera de tus proximas compras!',
-    expirationDate: '2021-08-31'
+    type: 'cupón',
+    value: '$15.000',
+    description: '¡Obten un descuento de $15.000 en cualquiera de tus próximas compras!',
+    expirationDate: '28 de Agosto de 2021'
   },
   {
     id: 4,
     type: 'oferta',
     value: '30%',
-    description: '¡Recibe un 30% de descuento en tu proxima compra!',
-    expirationDate: '2021-08-31'
+    description: '¡Recibe un 30% de descuento en tu próxima compra!',
+    expirationDate: '28 de Agosto de 2021'
   },
   {
     id: 5,
-    type: 'cupon',
-    value: '$ 10000',
-    description: '¡Obten un descuento de $10000 pesos en cualquiera de tus proximas compras!',
-    expirationDate: '2021-08-31'
+    type: 'cupón',
+    value: '$10.000',
+    description: '¡Obten un descuento de $10.000 en cualquiera de tus próximas compras!',
+    expirationDate: '28 de Agosto de 2021'
   },
   {
     id: 6,
-    type: 'cupon',
-    value: '$ 25000',
-    description: '¡Obten un descuento de $25000 pesos en cualquiera de tus proximas compras!',
-    expirationDate: '2021-08-31'
+    type: 'cupón',
+    value: '$25.000',
+    description: '¡Obten un descuento de $25.000 en cualquiera de tus próximas compras!',
+    expirationDate: '28 de Agosto de 2021'
   }
 ]
 
