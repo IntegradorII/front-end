0faa910798f54d69db7bd937d3cecc956d9d309b
camilaramirez4 <camila.ramirez4@udea.edu.co>
2023-11-25 16:00:01 -0500

    cCambio en paths de imagenes

--- src/components/common/nav-bar.tsx
+++ src/components/common/nav-bar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <nav className='w-full flex justify-center items-center'>
       <div className='flex'>
-        <Image src='/images/logo.png' alt='Offcorss Logo' width={204} height={70}/>
+        <Image src='/images/logo-offcors.png' alt='Offcorss Logo' width={204} height={70}/>
       </div>
       <div className='flex absolute right-2 gap-3'>
         <ButtonAuth />
