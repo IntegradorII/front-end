9bf9f9df441b868c05eca62e5d473e49617bd754
Jeison Moncayo <desarrollo5ude@udea.edu.co>
2023-11-28 06:23:03 -0500

    Camnios en el esquema

--- src/components/common/side-bar.tsx
+++ src/components/common/side-bar.tsx
@@ -1,9 +1,10 @@
+'use client'
 import Image from 'next/image'
 import { PercentageBar } from '@/components/home/percentage-bar'
 import { BsFillStarFill } from 'react-icons/bs'
 import { PiStarDuotone } from 'react-icons/pi'
 import { SideBarList, type LinkItem } from './side-bar-list'
-import { LinkWrapper } from './link-wrapper'
+import Link from 'next/link'
 
 const listItems: LinkItem[] = [
   {
@@ -34,44 +35,43 @@ const listItems: LinkItem[] = [
 
 const SideBar = () => {
   return (
-    <div className='w-[21rem] first-letter:flex flex justify-center fixed h-full border-r-separator-gray border-r-2 p-8'>
-      <div className='top-0 left-0 w-full flex h-fit'>
-        <div className='flex flex-col items-center gap-3 bg-white px-8 py-5 rounded-3xl text-sm'>
-          <LinkWrapper url='/dashboard/user-profile' className='w-full flex flex-col items-center'>
-            <div className='w-full flex flex-col items-center'>
-              <Image src="/images/profile.png" alt="Foto de perfil" width={90} height={90} className='rounded-full'/>
-              <div className='flex flex-col items-center font-bold text-base'>
-                <p>Hola,</p>
-                <p>Juanita Pérez</p>
-              </div>
-              <div className='w-full flex items-center gap-2'>
-                <div className='text-yellow text-lg'>
-                  <PiStarDuotone/>
-                </div>
-                <div className='w-full'>
-                  <PercentageBar percentage={70}/>
-                </div>
-                <div className='text-yellow text-lg'>
-                  <BsFillStarFill/>
-                </div>
-              </div>
+    <aside id='my-custom-sidebar'
+      className='w-[21rem] flex justify-start items-start fixed top-[5rem] h-[calc(100vh-5rem)] p-8 overflow-y-auto'>
+      <div className='flex flex-col items-center gap-3 bg-white px-8 py-5 rounded-3xl text-sm'>
+        <Link href='/dashboard/user-profile' className='w-full flex flex-col items-center'>
+          <div className='w-full flex flex-col items-center'>
+            <Image src="/images/profile.png" alt="Foto de perfil" width={90} height={90} className='rounded-full'/>
+            <div className='flex flex-col items-center font-bold text-base'>
+              <p>Hola,</p>
+              <p>Juanita Pérez</p>
             </div>
-          </LinkWrapper>
-          <div className='flex flex-col items-center text-center bg-medium-gray rounded-xl font-semibold text-white px-10 py-1 my-4'>
-            <div className='flex gap-1'>
-              <span>Eres</span>
-              <span className='text-yellow'>Star</span>
-            </div>
-            <span>Te faltan 300 puntos</span>
-            <div className='flex gap-1'>
-              <span>para ser</span>
-              <span className='text-yellow'>CoolStar</span>
+            <div className='w-full flex items-center gap-2'>
+              <div className='text-yellow text-lg'>
+                <PiStarDuotone/>
+              </div>
+              <div className='w-full'>
+                <PercentageBar percentage={70}/>
+              </div>
+              <div className='text-yellow text-lg'>
+                <BsFillStarFill/>
+              </div>
             </div>
           </div>
-          <SideBarList items={listItems}/>
+        </Link>
+        <div className='flex flex-col items-center text-center bg-medium-gray rounded-xl font-semibold text-white px-6 py-2 my-3'>
+          <div className='flex gap-1'>
+            <span>Eres</span>
+            <span className='text-yellow'>Star</span>
+          </div>
+          <span>Te faltan 300 puntos</span>
+          <div className='flex gap-1'>
+            <span>para ser</span>
+            <span className='text-yellow'>CoolStar</span>
+          </div>
         </div>
+        <SideBarList items={listItems}/>
       </div>
-    </div>
+    </aside>
   )
 }
 
