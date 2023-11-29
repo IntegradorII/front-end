dce36853031ba6f0940a4754649d8d44bcffbfea
Jeison Moncayo <desarrollo5ude@udea.edu.co>
2023-11-28 20:22:38 -0500

    Corregido alto de componente

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,8 +8,8 @@ export default function DashboardLayout ({ children }: DashboardLayoutProps) {
   return (
     <section className='w-full min-h-[calc(100vh-80px)]'>
       <SideBar />
-      <section className='pl-[21rem]'>
-        <main className='w-full h-full flex items-start py-10 px-16 border-l-separator-gray border-l-2'>
+      <section className='pl-[21rem] h-full'>
+        <main className='w-full min-h-[calc(100vh-80px)] flex items-start py-10 px-16 border-l-separator-gray border-l-2'>
           {children}
         </main>
       </section>
