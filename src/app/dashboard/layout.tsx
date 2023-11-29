fe431deb999d510be36339fab5d85324de5ad99c
Jeison Moncayo <desarrollo5ude@udea.edu.co>
2023-11-29 09:20:16 -0500

    Ajustado layouts

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { SideBar } from '@/components/common/side-bar'
+import { ClientWrapper } from '@/components/common/client-wrapper'
 
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -6,13 +7,15 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout ({ children }: DashboardLayoutProps) {
   return (
-    <section className='w-full min-h-[calc(100vh-80px)]'>
-      <SideBar />
-      <section className='pl-[21rem] h-full'>
-        <main className='w-full min-h-[calc(100vh-80px)] flex items-start py-10 px-16 border-l-separator-gray border-l-2'>
-          {children}
-        </main>
-      </section>
+    <section className='w-full max-w-[1900px] mx-auto min-h-[calc(100vh-80px)]'>
+      <ClientWrapper>
+        <SideBar />
+        <section className='pl-[21rem] h-full'>
+          <main className='w-full min-h-[calc(100vh-80px)] flex items-start py-10 pl-12 pr-8 border-l-separator-gray border-l-2'>
+            {children}
+          </main>
+        </section>
+      </ClientWrapper>
     </section>
   )
 }
--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,6 +1,7 @@
 const Footer = () => {
   return (
-    <footer className='w-full h-11 bg-dark-gray text-white text-sm flex items-center justify-between px-5 z-10 absolute'>
+    <footer id='my-custom-footer'
+      className='w-full h-11 bg-dark-gray text-white text-sm flex items-center justify-between px-5 z-10 absolute'>
       <div>@CopyRight</div>
     </footer>
   )
